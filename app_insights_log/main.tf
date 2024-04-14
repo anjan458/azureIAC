9113421b4eac99664db10a925384798b0ce785e9
anjan458 <166825794+anjan458@users.noreply.github.com>
2024-04-14 19:59:02 +0530

    log_analytics_workspace

--- app_insights_log/main.tf
+++ app_insights_log/main.tf
@@ -8,14 +8,10 @@ resource "azurerm_log_analytics_workspace" "law" {
   resource_group_name                = data.azurerm_resource_group.rg.name
   sku                                = each.value["sku"]
   retention_in_days                  = each.value["retention_in_days"] == null ? null : each.value["retention_in_days"]
-  allow_resource_only_permissions    = each.value["allow_resource_only_permissions"] == null ? null : each.value["allow_resource_only_permissions"]
-  local_authentication_disabled      = each.value["local_authentication_disabled"] == null ? null : each.value["local_authentication_disabled"]
   daily_quota_gb                     = each.value["daily_quota_gb"] == null ? null : each.value["daily_quota_gb"]
-  cmk_for_query_forced               = each.value["cmk_for_query_forced"] == null ? null : each.value["cmk_for_query_forced"]
   internet_ingestion_enabled         = each.value["internet_ingestion_enabled"] == null ? null : each.value["internet_ingestion_enabled"]
   internet_query_enabled             = each.value["internet_query_enabled"] == null ? null : each.value["internet_query_enabled"]
   reservation_capacity_in_gb_per_day = each.value["reservation_capacity_in_gb_per_day"] == null ? null : each.value["reservation_capacity_in_gb_per_day"]
-  data_collection_rule_id            = each.value["data_collection_rule_id"] == null ? null : each.value["data_collection_rule_id"]
 }
 resource "azurerm_application_insights" "app_insights" {
   for_each                              = var.application_insights
--- app_insights_log/variables.tf
+++ app_insights_log/variables.tf
@@ -32,15 +32,10 @@ variable "log_analytics_workspace" {
     name                               = string
     sku                                = string
     retention_in_days                  = optional(number)
-    allow_resource_only_permissions    = optional(bool)
-    local_authentication_disabled      = optional(bool)
     daily_quota_gb                     = optional(string)
-    cmk_for_query_forced               = optional(string)
     internet_ingestion_enabled         = optional(bool)
     internet_query_enabled             = optional(bool)
     reservation_capacity_in_gb_per_day = optional(string) #The capacity reservation level in GB for this workspace. Possible values are 100, 200, 300, 400, 500, 1000, 2000 and 5000.
-    data_collection_rule_id            = optional(string)
-    tags                               = optional(string)
   }))
   default = {
     "one" = {
