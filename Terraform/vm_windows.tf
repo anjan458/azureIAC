321ac8f37c752adb81fed9774e9bfa1ef476bae9
anjan458 <166825794+anjan458@users.noreply.github.com>
2024-04-18 20:21:48 +0530

    Add files via upload

---
+++ Terraform/vm_windows.tf
@@ -0,0 +1,11 @@
+module "" {
+  source            = ""
+  vm_rg             = ""
+  vm_vnet           = ""
+  vm_snet           = ""
+  vm_name           = ""
+  vm_size           = ""
+  vm_admin_username = ""
+  vm_admin_password = ""
+  vm_c_disk_size    = ""
+}
